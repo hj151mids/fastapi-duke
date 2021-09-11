b5200307b2ad2c2e848411a37939efb112c9c234
EC2 Default User <ec2-user@ip-172-31-28-173.ec2.internal>
2021-09-11 03:34:28 +0000

    multiply initial api

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to IDS 706"}
+    
+@app.get("/multiply/{num1}/{num2}")
+async def multiply(num1: int, num2: int):
+    total = num1 * num2
+    return {"total": total}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host='0.0.0.0')
